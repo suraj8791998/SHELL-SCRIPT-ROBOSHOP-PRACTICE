6b8192d6d617f3fee92318a8f23f8aee6422d38b
hemasuraj879 <your_email@example.com>
2024-02-08 20:13:59 +0530

    files pushing

--- cart.sh
+++ cart.sh
@@ -51,7 +51,7 @@ VALIDATE $? "UNZIPPING THE APPLICATION"
 npm install  &>> $LOGFILE
 VALIDATE $? "INSTALLING THE DEPENDENCIES"
 
-cp /home/centos/SHELL-SCRIPT-ROBOSHOP-PRACTICE/cart.service /etc/systemd/system/cart.service  &>> $LOGFILE
+cp //home/centos/SHELL-SCRIPT-ROBOSHOP-PRATICE/cart.service /etc/systemd/system/cart.service  &>> $LOGFILE
 VALIDATE $? "COPYING CART SERVICE"
 
 systemctl daemon-reload &>> $LOGFILE
